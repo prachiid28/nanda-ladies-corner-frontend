d8f5f7753225f22bf863190f7f455f72dc5d66b8
prachii-web <deshmukhprachi225@gmail.com>
2026-07-30 21:08:31 +0530

    Add JWT token to API requests

--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,18 @@ const api = axios.create({
     baseURL: "https://nanda-ladies-corner-backend.onrender.com/api"
 });
 
+
+api.interceptors.request.use((config)=>{
+
+    const token = localStorage.getItem("token");
+
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+
+});
+
+
 export default api;
